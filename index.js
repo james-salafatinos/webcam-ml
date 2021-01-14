6d107d4605d949c56cccd5ece226d2f6fe8c2018
jfs <jamessalafatinos@outlook.com>
2021-01-13 20:08:03 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const start = async () => {
+    
+    const createKNNClassifier = async () => {
+        
+    }
+   
+    
+    const createMobileNetModel = async () => {
+        
+    }
+   
+    
+    const createWebcamInput = async () => {
+        
+    }
+};
+
+
+
+window.onload = () => {...};
+//callback function when page loads as there
+//is additional processing time to build model and unpackage mobilenet weights
